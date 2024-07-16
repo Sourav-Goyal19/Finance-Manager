86110a883808db53394c5da49e0b817e4be2a6e1
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-07-16 20:25:09 +0530

    Skeleton Update

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import { Metadata } from "next";
 import TransactionsPageClient from "./components/transactions-client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Loader2 } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const metadata: Metadata = {
   title: "Transactions | FinFlow",
@@ -18,9 +21,17 @@ const TransactionsPage = () => {
 
 const TransactionsPageFallback = () => {
   return (
-    <div>
-      <h1>Transactions</h1>
-      <p>Loading transactions...</p>
+    <div className="max-w-screen-2xl mx-auto w-full -mt-24 pb-10">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader>
+          <Skeleton className="h-8 w-48" />
+        </CardHeader>
+        <CardContent>
+          <div className="h-[500px] w-full flex items-center justify-center">
+            <Loader2 className="size-12 text-slate-300 animate-spin" />
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
